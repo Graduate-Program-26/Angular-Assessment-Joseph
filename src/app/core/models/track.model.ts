01f450cb29dd2a3c17af0726827a197128832604
Joseph-Monakedi <Jmonakedi@dvtsoftware.com>
2026-05-05 19:00:48 +0200

    feat(models): consolidate track and album interfaces

--- src/app/core/models/track.model.ts
+++ src/app/core/models/track.model.ts
@@ -13,14 +13,7 @@ export interface Track {
     picture_medium?: string;
     picture_big?: string;
   };
-  album: {
-    id: number;
-    title: string;
-    cover: string;
-    cover_small: string;
-    cover_medium: string;
-    cover_big: string;
-  };
+  album: Album;
 }
 
 export interface Playlist {
@@ -43,10 +36,14 @@ export interface Album {
   id: number;
   title: string;
   cover_medium: string;
+  cover_small: string;
+  cover_big: string;
+  tracklist: string;
   artist: {
     name: string;
   };
   type: 'album';
+  tracks: {data: Track[], total: number};
 }
 
 export type SearchItem = Track | Artist | Album;
